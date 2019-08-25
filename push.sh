4bfa9f446c7cf38f62b9580f98adcef42373f617
freshchen <961011595@qq.com>
2019-08-25 19:15:02 +0800

    notes

--- push.sh
+++ push.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+current_path=$(cd "$(dirname "$0")"; pwd)
+cd ${current_path}
+status=$(git pull)
+if [[ ${status} == "Already up to date." ]]; then
+    echo 'Start to push.'
+    git add ./docs/ || exit 1
+    git commit -m 'notes' || exit 1
+    git push origin master || exit 1
+    echo 'Push successfully.'
+else
+    echo 'Need merge.'
+fi
