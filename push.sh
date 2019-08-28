b75e1c427dbc499cf4e98c8536888f10227845e9
freshchen <961011595@qq.com>
2019-08-28 14:46:53 +0800

    [fresh] update

--- push.sh
+++ push.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+if [[ $# -eq 0 ]] ; then
+    comment="update"
+else
+    comment=$*
+fi
 current_path=$(cd "$(dirname "$0")"; pwd)
 cd ${current_path}
 status=$(git pull)
 if [[ ${status} == "Already up to date." ]]; then
     echo 'Start to push.'
     git add ./docs/ || exit 1
-    git commit -m 'notes' || exit 1
+    git commit -m "${comment}" || exit 1
     git push origin master || exit 1
     echo 'Push successfully.'
 else
